58516bc5394e9d2bf154078f20f0742ef4688f36
rashadat1 <rashadat@yahoo.com>
2024-07-08 02:41:49 -0400

    added a fourth pokemon to FeaturedPokemon component

--- pokemon-frontend/src/components/FeaturedPokemon.js
+++ pokemon-frontend/src/components/FeaturedPokemon.js
@@ -6,7 +6,8 @@ const FeaturedPokemon = () => {
     const featured = [
         {id: 1, name: 'Bulbasaur', description: 'A grass/poison type pokemon and one of the Kanto region starters.'},
         {id: 2, name: 'Charmander', description: 'A fire type pokemon and one of the Kanto region starters.'},
-        {id: 3, name: 'Squirtle', description: 'A water type pokemon and one of the Kanto region starters.'}
+        {id: 3, name: 'Squirtle', description: 'A water type pokemon and one of the Kanto region starters.'},
+        {id: 4, name: 'Pikachu', description: 'An electric type pokemon native to the Kanto region.'}
     ];
 
     return (
